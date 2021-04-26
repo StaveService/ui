7d1ec7a7024bc585fbb244d1da145faa58ff24d0
izszzz <izszzz@yahoo.co.jp>
2021-04-26 13:16:22 +0900

    ui update

--- Layout/index.tsx
+++ Layout/index.tsx
@@ -1,13 +1,11 @@
-import React, { ChangeEvent } from "react";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
-import { LayoutMode } from "@coderline/alphatab";
+import React, { ChangeEvent } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 
-const { Horizontal, Page } = LayoutMode;
 interface LayoutProps {
-  layout?: typeof Horizontal | typeof Page;
+  layout?: 0 | 1;
   onLayout?: (e: ChangeEvent<{ name?: string; value: unknown }>) => void;
 }
 const Layout: React.FC<LayoutProps> = ({ onLayout, layout }: LayoutProps) => {
@@ -21,14 +19,14 @@ const Layout: React.FC<LayoutProps> = ({ onLayout, layout }: LayoutProps) => {
         onChange={onLayout}
         autoWidth
       >
-        <MenuItem value={Horizontal}>Horizontal</MenuItem>
-        <MenuItem value={Page}>Page</MenuItem>
+        <MenuItem value={0}>Horizontal</MenuItem>
+        <MenuItem value={1}>Page</MenuItem>
       </Select>
     </FormControl>
   );
 };
 Layout.defaultProps = {
-  layout: Page,
+  layout: 0,
   onLayout: undefined,
 };
 export default Layout;
